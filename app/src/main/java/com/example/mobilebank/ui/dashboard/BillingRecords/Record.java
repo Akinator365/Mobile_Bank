e007d1d2627f5b80b841b008cf8aaa51b06231e4
HHL <1743533928@qq.com>
2021-12-22 23:42:43 +0800

    添加<收支记录>按钮

---
+++ app/src/main/java/com/example/mobilebank/ui/dashboard/BillingRecords/Record.java
@@ -0,0 +1,17 @@
+package com.example.mobilebank.ui.dashboard.BillingRecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.example.mobilebank.R;
+
+public class Record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
